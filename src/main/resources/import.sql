adf4509f014e4bffc509dbaa0dd29f05f2104cb3
EduardoMonti <dudumonti@hotmail.com>
2024-08-13 12:10:11 -0300

    Adding import.sql in resources

---
+++ src/main/resources/import.sql
@@ -0,0 +1,7 @@
+INSERT INTO tb_department(name) VALUES ('Management');
+INSERT INTO tb_department(name) VALUES ('Computers');
+
+INSERT INTO tb_user(department_id, name, email) VALUES (1, 'Marie', 'marie@gmail.com');
+INSERT INTO tb_user(department_id, name, email) VALUES (1, 'Bob', 'bob@gmail.com');
+INSERT INTO tb_user(department_id, name, email) VALUES (2, 'Alex', 'alex@gmail.com');
+INSERT INTO tb_user(department_id, name, email) VALUES (2, 'Ana', 'ana@gmail.com');
