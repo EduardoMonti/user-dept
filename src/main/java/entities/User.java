858955d1e9fb8f8e8f576a148034a393b4287057
EduardoMonti <dudumonti@hotmail.com>
2024-08-13 10:38:23 -0300

    Adding annotations in User and Department

--- src/main/java/entities/User.java
+++ src/main/java/entities/User.java
@@ -1,7 +1,15 @@
 package entities;
 
+
+import jakarta.persistence.*;
+
+
+@Entity
+@Table(name = "tb_user")
 public class User {
 
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
     private String name;
     private String email;
