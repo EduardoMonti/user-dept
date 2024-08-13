7858886b87dd5653a4db95278a672ead0f4e6874
EduardoMonti <dudumonti@hotmail.com>
2024-08-13 15:07:49 -0300

    Adding findById and insert

--- src/main/java/com/example/userdept/controllers/UserController.java
+++ src/main/java/com/example/userdept/controllers/UserController.java
@@ -2,9 +2,7 @@ package com.example.userdept.controllers;
 
 import com.example.userdept.entities.User;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 import com.example.userdept.repository.UserRepository;
 
 import java.util.List;
@@ -18,7 +16,20 @@ public class UserController {
 
     @GetMapping
     public List<User> findAll() {
-        return userRepository.findAll();
+        List <User> users = userRepository.findAll();
+        return users;
+    }
+
+    @GetMapping(value = "/{id}")
+    public User findById(@PathVariable Long id){
+        User user = userRepository.findById(id).get();
+        return user;
+    }
+
+    @PostMapping
+    public User insert(@RequestBody User user){
+        User saved = userRepository.save(user);
+        return user;
     }
 
 }
