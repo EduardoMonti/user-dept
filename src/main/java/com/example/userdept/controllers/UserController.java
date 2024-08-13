f2d62797e316eef758e637537ea901940d0ca93f
EduardoMonti <dudumonti@hotmail.com>
2024-08-13 15:56:09 -0300

    Adding delete

--- src/main/java/com/example/userdept/controllers/UserController.java
+++ src/main/java/com/example/userdept/controllers/UserController.java
@@ -2,10 +2,12 @@ package com.example.userdept.controllers;
 
 import com.example.userdept.entities.User;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import com.example.userdept.repository.UserRepository;
 
 import java.util.List;
+import java.util.Optional;
 
 @RestController
 @RequestMapping(value = "/users")
@@ -16,20 +18,33 @@ public class UserController {
 
     @GetMapping
     public List<User> findAll() {
-        List <User> users = userRepository.findAll();
+        List<User> users = userRepository.findAll();
         return users;
     }
 
     @GetMapping(value = "/{id}")
-    public User findById(@PathVariable Long id){
+    public User findById(@PathVariable Long id) {
         User user = userRepository.findById(id).get();
         return user;
     }
 
     @PostMapping
-    public User insert(@RequestBody User user){
+    public User insert(@RequestBody User user) {
         User saved = userRepository.save(user);
         return user;
+    }
+
+    @DeleteMapping(value = "/{id}")
+    public ResponseEntity<User> deleteById(@PathVariable Long id) {
+        Optional<User> userOptional = userRepository.findById(id);
+
+        if (userOptional.isPresent()) {
+            User user = userOptional.get();
+            userRepository.deleteById(id);
+            return ResponseEntity.ok(user);
+        } else {
+            return ResponseEntity.notFound().build();
+        }
     }
 
 }
